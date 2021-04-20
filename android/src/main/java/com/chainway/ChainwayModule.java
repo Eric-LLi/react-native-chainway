ee53afe875503f5cd7c064ea95ffbab5761199f0
Eric-LLi <ericli1116@gmail.com>
2021-04-20 18:10:26 +1000

    <UPDATE>

--- android/src/main/java/com/chainway/ChainwayModule.java
+++ android/src/main/java/com/chainway/ChainwayModule.java
@@ -2,14 +2,24 @@
 
 package com.chainway;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
 public class ChainwayModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
 
     private final ReactApplicationContext reactContext;
+
+    private final String LOG = "[CHAINWAY]";
+    private final String READER_STATUS = "READER_STATUS";
+    private final String TRIGGER_STATUS = "TRIGGER_STATUS";
+    private final String WRITE_TAG_STATUS = "WRITE_TAG_STATUS";
+    private final String BATTERY_STATUS = "BATTERY_STATUS";
+    private final String TAG = "TAG";
+    private final String LOCATE_TAG = "LOCATE_TAG";
+
+    private static RFIDWithUHF mReader = null;
 
     public ChainwayModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -47,5 +57,23 @@ public class ChainwayModule extends ReactContextBaseJavaModule implements Lifecy
     @Override
     public void onHostDestroy() {
         //
+    }
+
+    @ReactMethod
+    public void isConnected(Promise promise) {
+        //
+    }
+
+    private void doConnect() {
+        //
+    }
+
+    private void doDisconnect() {
+        if(mReader != null) {
+            mReader.free();
+
+            scannedTags = new ArrayList<>();
+			mReader = null;
+        }
     }
 }
